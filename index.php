18d2472a8db087dcde844dd5f7ba9dee0d2e6a87
mich <michelson@poucedigital.com>
2018-10-30 16:15:31 +0100

    content

--- index.php
+++ index.php
@@ -4,68 +4,72 @@
   include __DIR__ . '/layout/nav.php';
  ?>
  <main>
-     <section class="hero is-info is-fullheight">
+     <section class="hero is-info is-fullheight" id="cover">
          <div class="hero-body">
              <div class="container">
-                 <div class="column is-6">
-                     <h2 class="title">Come on gurls, let's code ! *</h2>
-                     <p class="subtitle">Nous accompagnons les femmes des <strong>Quartiers Prioritaires</strong> à être connectées et à s'émanciper grace à l’<strong>inclusion numérique</strong>.</p>
+                 <div class="column is-4 is-offset-8">
+                     <h2 class="title"> Qui sont-elles ?</h2>
+                     <p class="subtitle"> Des Femmes. Dans les Quartiers. Connectées, emancipées, elles vont à l'assaut du numérique ! </p>
                  </div>
              </div>
          </div>
          <div class="hero-foot">
-            <div class="container">
-                * On y va les filles, on code !
-            </div>
+           <div class="container">
+             <div class="column is-4 is-offset-8">
+             </div>
+           </div>
          </div>
      </section>
      <!-- chiffres -->
      <section class="section container is-medium">
-         <h2 class="title has-text-centered">32 533 153 femmes en France</h2>
+         <div class="section">
+             <h2 class="title has-text-centered">1,6 millions d’habitants en QPV</h2>
+              <p class="heading has-text-centered">en Île de France</p>
+         </div>
          <div class="hero is-medium">
              <div class="tile is-ancestor">
                  <div class="tile is-parent">
                      <article class="tile is-child has-text-centered">
-                         <p class="title"><i class="fas fa-female"></i> <i class="fas fa-female"></i> <i class="fas fa-female"></i>  <i class="fas fa-female"></i></p>
-                         <p class="title">28 422 millions</p>
-                         <p class="heading">Actives</p>
+                         <p class="title"><i class="fas fa-male"></i> <i class="fas fa-female"></i></p>
+                         <p class="title">52%</p>
+                         <p class="heading">Femmes</p>
                      </article>
                  </div>
                  <div class="tile is-parent">
                      <article class="tile is-child has-text-centered">
-                         <p class="title"><i class="far fa-frown"></i> <i class="far fa-frown"></i> <i class="far fa-frown"></i></p>
-                         <p class="title">1 sur 4</p>
-                         <p class="heading">Sans emploi</p>
+                         <p class="title"><i class="far fa-smile"></i> <i class="far fa-frown"></i></p>
+                         <p class="title">1 sur 2</p>
+                         <p class="heading">Enloignée de l'emploi</p>
+                     </article>
+                 </div>
+                 <div class="tile is-parent">
+                     <article class="tile is-child has-text-centered">
+                         <p class="title"><i class="fas fa-search"></i> <i class="fas fa-desktop"></i></p>
+                         <p class="title">191 000</p>
+                         <p class="heading">cherchent emploi</p>
                      </article>
                  </div>
                  <div class="tile is-parent">
                      <article class="tile is-child has-text-centered">
                          <p class="title"><i class="fas fa-desktop"></i> <i class="fas fa-desktop"></i></p>
-                         <p class="title">5 5097</p>
-                         <p class="heading">Emplois dans le web</p>
-                     </article>
-                 </div>
-                 <div class="tile is-parent">
-                     <article class="tile is-child has-text-centered">
-                         <p class="title"><i class="fas fa-female"></i> <i class="fas fa-desktop"></i></p>
-                         <p class="title">1 500</p>
-                         <p class="heading">Développeuses actuelement</p>
+                         <p class="title">80.000</p>
+                         <p class="heading">Postes à pourvoir dans le web</p>
                      </article>
                  </div>
              </div>
          </div>
      </section>
+
      <!-- ambition -->
      <section class="section hero is-info" id="ambitions">
          <div class="columns">
-             <div class="column is-4 is-offset-5">
-                 <h2 class="title"> - Nous manquons de formation et de qualification - </h2>
-                 <p>En France, nous representons <strong>4,8 millions d'habitants</strong>. nous vivons au sein des <strong>1.296 quartiers</strong> de la politique de la ville. Notre population est fréquemment touchée par la pauvreté. Nos habitants sont plus jeunes qu'en moyenne dans la population urbaine. Nous sommes souvent nés de familles nombreuses ou monoparentales ou d'étrangers. Au dela de la pauvreté, nous rencontrons de grandes difficultes a nous inserer sur le marche du travail et manquons de <strong>formation et de qualification.</strong></p>
-
+             <div class="column is-8 is-offset-2">
+                 <h2 class="title">notre ambition : être un acteur majeur de l’accompagnement des femmes des quartiers politique de la ville, à intégrer le numérique.</h2>
                  <p class="has-text-right"><span>DesCodeuses</span></p>
              </div>
          </div>
      </section>
+
      <!-- actions -->
      <section class="section container has-text-centered" id="actions">
          <h2 class="title">Nos actions</h2>
@@ -79,8 +83,8 @@
                      </defs>
                      <polygon points="50 1 95 25 95 75 50 99 5 75 5 25" fill="url(#img)"/>
                  </svg>
-                 <h2>Mentorat</h2>
-                 <p>Organiser des rencontres entre femmes (rôles modèles) et femmes débutantes, étrangères à cette nouvelle économie </p>
+                 <h2>sensibilisation</h2>
+                 <p>Sensibiliser les femmes des quartiers politique de la ville aux opportunités d’emploi dans le numérique</p>
              </div>
              <div class="column is-4 has-text-centered">
                  <svg viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
@@ -91,8 +95,8 @@
                      </defs>
                      <polygon points="50 1 95 25 95 75 50 99 5 75 5 25" fill="url(#img)"/>
                  </svg>
-                 <h2>Inclusion numérique</h2>
-                 <p>Offrir des ateliers d’initiation ou perfectionnement de l’utilisation des outils numériques</p>
+                 <h2>Formation</h2>
+                 <p>Former les femmes des quartiers politique de la ville aux métiers du développement web</p>
              </div>
              <div class="column is-4 has-text-centered">
                  <svg viewBox="0 0 100 100" version="1.1" xmlns="http://www.w3.org/2000/svg">
@@ -103,8 +107,8 @@
                      </defs>
                      <polygon points="50 1 95 25 95 75 50 99 5 75 5 25" fill="url(#img)"/>
                  </svg>
-                 <h2>Professionnalisante</h2>
-                 <p>Former aux métiers du développement web/application</p>
+                 <h2>Accompagnement</h2>
+                 <p>Accompagner les femmes des quartiers politique de la ville jusqu’à l’emploi</p>
              </div>
          </div>
          </div>
