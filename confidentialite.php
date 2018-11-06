5a46357d490ec53567807c133dcf2330cd8159b2
mich <michelson@poucedigital.com>
2018-11-06 16:52:55 +0100

    politique de confidentialité content

--- confidentialite.php
+++ confidentialite.php
@@ -5,7 +5,132 @@
  ?>
 
  <main>
+     <div id="politique">
+         <section class="hero section is-info is-medium">
+             <div class="container has-text-centered">
+                 <h1 class="title">Politique de confidentialité</h1>
+                 <p class="subtitle">Soyons clair !</p>
+             </div>
+         </section>
+         <div class="container">
+             <section class="section">
+                 <p>Vous trouverez ici l’ensemble des informations nécessaires pour comprendre comment <span>DesCodeuses</span>, traite et protège vos données dans le cadre de l’utilisation de ses services sur le site internet <strong><a target="_blank"  href="https://www.descodeuses.org">https://www.descodeuses.org</a></strong></p>
 
+                 <p>L’utilisation du présent site est soumise à la présente Politique.</p>
+
+                 <p>Compte tenu de l’évolution constante des lois et règlements en matière de technologie et de protection des données à caractère personnel, il est probable que la présente Politique soit mise à jour.</p>
+
+                 <p>Si l’une quelconque des clauses de la présente Politique devait être déclarée nulle ou contraire à la réglementation, elle sera réputée non écrite mais n’entraînera pas la nullité des autres clauses de la présente Politique.</p>
+
+                 <p>Nous vous remercions de lire attentivement la Politique pour comprendre clairement nos pratiques concernant le traitement de vos données et vous informons que la consultation et/ou l’utilisation de notre site entraîne nécessairement l’acceptation de la présente Politique.</p>
+
+                 <p>Si cette dernière n’obtient pas votre adhésion, nous vous invitons à ne pas utiliser notre site.</p>
+
+                 <p>Il vous appartient de vous assurer que les données communiquées à <span>DesCodeuses</span> sont complètes et à jour.</p>
+
+                 <h2 class="title2">Qui est votre Référent au sein de <span>DesCodeuses</span> pour toute question sur vos données ?</h2>
+                 <p>Michelson NOVAES SANTOS NANTIER, co-fondateur de l’association <span>DesCodeuses</span>, est votre Référent Informatique et Libertés chargé des questions relatives à la protection des données à caractère personnel.</p>
+
+                 <p>Pour toute question relative à vos données et ce qui en est fait par l’association <span>DesCodeuses</span> vous pouvez contacter le Référent Informatique et Libertés par email <strong><a target="_blank"  href="https://wwww.descodeuses.org/index.php#contact">https://wwww.descodeuses.org/index.php#contact</a></strong> ou par courrier à l’adresse suivante : 37 Rue du Télégraphe 75020.</p>
+
+                 <h2 class="title2">A quelles fins les données sont-elles collectées ?</h2>
+
+                 <p><span>DesCodeuses</span> utilise vos données pour répondre à vos demandes et vous permettre de bénéficier de ses services tels que : la demande d’informations concernant services. Certaines données peuvent également être utilisées pour analyser le trafic du site et les habitudes de navigation ou de commande, nous aider à comprendre la manière dont vous utilisez le site afin de l’améliorer.</p>
+
+                 <h2 class="title2">Comment vos données sont-elles collectées ?</h2>
+                 <ul>
+                     <li>Les données peuvent être collectées par différents moyens depuis le site.</li>
+                     <li>Formulaire de contact</li>
+                     <li>Lorsque vous nous les communiquez au moyen des formulaires de contact.</li>
+                     <li>Cookies</li>
+                 </ul>
+                 <p>Vous pouvez à tout moment désactiver les cookies enregistrés sur votre terminal. Pour cela, il suffit de sélectionner les paramètres appropriés dans votre navigateur. Cependant, cette désactivation aura pour conséquence d’empêcher l’accès à certaines fonctionnalités du site permettant de personnaliser les services proposés par <span>DesCodeuses</span></p>
+
+                 <h2 class="title2">Contenu embarqué depuis d’autres sites</h2>
+
+                 <p>Les pages de ce site peuvent inclure des contenus intégrés (par exemple des vidéos, images, articles…). Le contenu intégré depuis d’autres sites se comporte de la même manière que si le visiteur se rendait sur cet autre site.</p>
+
+                 <p>Ces sites web pourraient collecter des données sur vous, utiliser des cookies, embarquer des outils de suivis tiers, suivre vos interactions avec ces contenus embarqués si vous disposez d’un compte connecté sur leur site web.</p>
+
+                 <h2 class="title2">Statistiques et mesures d’audience</h2>
+
+                 <p>
+                     Ce site utilise Google Analytics, un service d’analyse de site internet fourni par Google Inc. Google Analytics utilise des cookies, qui sont des fichiers texte placés sur votre ordinateur, pour aider le site internet à analyser l’utilisation du site par ses utilisateurs. Les données générées par les cookies concernant votre utilisation du site (y compris votre adresse IP) seront transmises et stockées par Google sur un de leurs serveurs situés aux Etats-Unis. Google utilisera cette information dans le but d’évaluer votre utilisation du site, de compiler des rapports sur l’activité du site à destination de son éditeur et de fournir d’autres services relatifs à l’activité du site et à l’utilisation d’Internet. Google est susceptible de communiquer ces données à des tiers en cas d’obligation légale ou lorsque ces tiers traitent ces données pour le compte de Google, y compris notamment l’éditeur de ce site. Google ne recoupera pas votre adresse IP avec toute autre donnée détenue par Google. Vous pouvez désactiver l’utilisation de cookies en sélectionnant les paramètres appropriés de votre navigateur. Cependant, une telle désactivation pourrait empêcher l’utilisation de certaines fonctionnalités de ce site. En utilisant ce site internet, vous consentez expressément au traitement de vos données nominatives par Google dans les conditions et pour les finalités décrites ci-dessus.
+                 </p>
+                 <p>En savoir plus, rendez-vous sur la page Google RGPD : <strong><a target="_blank"  href="https://privacy.google.com/intl/fr_fr/businesses/compliance/">https://privacy.google.com/intl/fr_fr/businesses/compliance/</a></strong> </p>
+
+                 <h2 class="title2">Boutons de partage (Twitter, Facebook, Instagram)</h2>
+
+                 <p>Le site <span>DesCodeuses</span> s’appuie sur certains services proposés par des sites tiers pour améliorer l’interactivité du site : Twitter, Facebook, Instagram. <span>DesCodeuses</span> n’a aucune influence sur la façon dont ces sites tiers utilisent vos données personnelles. C’est pourquoi, vous êtes invités à vous reporter à leur politique de confidentialité.</p>
+
+                 <p>En savoir plus, rendez-vous les pages RGPD des services concernés :</p>
+                 <ul>
+                     <li>Facebook : <a target="_blank"  href="https://www.facebook.com/about/privacy">https://www.facebook.com/about/privacy</a></li>
+                     <li>Twitter : <a target="_blank"  href="https://twitter.com/privacy?lang=fr">https://twitter.com/privacy?lang=fr</a></li>
+                     <li>Instagram : <a target="_blank"  href="https://help.instagram.com/519522125107875">https://help.instagram.com/519522125107875</a></li>
+                 </ul>
+
+                 <h2 class="title2">Quelles données sont collectées par <span>DesCodeuses</span>?</h2>
+
+                 <p>Selon les interactions effectuées (contact, navigation) nous sommes susceptibles de collecter les données suivantes :</p>
+
+                 <p>Votre adresse e-mail vous sera nécessairement demandée sur l’ensemble de nos formulaires de collecte et plus précisément, si vous souhaitez :</p>
+                 <p>– nous contacter via le formulaire de contact,</p>
+
+                 <p>Les données suivantes relatives à votre identité, pourront vous être demandées, telles que :  votre nom et prénom, adresse postale, adresse électronique, numéro de téléphone.</p>
+
+                 <p>Les données relatives à la navigation : il s’agit des données relatives à la manière dont vous utilisez le site comprenant : l’adresse IP, le navigateur utilisé, la durée de navigation, l’historique des recherches, le système d’exploitation utilisé, la langue et les pages visualisées ;</p>
+
+                 <p>Les données concernant vos visites sur notre site, y compris les données de trafic, les fichiers de journal et autres données ou ressources de communication que vous utilisez en accédant à notre site ;</p>
+
+                 <p>Les données relatives à l’utilisation des réseaux sociaux lorsque vous en utilisez les fonctionnalités.</p>
+
+                 <h2 class="title2">Quels sont les destinataires des données ?</h2>
+                 <p>Les données collectées sont destinées à l’usage de <span>DesCodeuses</span></p>
+
+                 <h2 class="title2">Combien de temps vos données sont-elles conservées ?</h2>
+
+                 <p>Le Parcours ne conserve vos données que pour la durée nécessaire aux finalités exposées à l’Article 3 du règlement européen 2016/679 sur la protection des données.</p>
+
+                 <p>Cette durée de conservation n’est pas la même selon les données en cause, la nature et la finalité de la collecte étant susceptible de faire varier cette durée. De même, certaines obligations légales imposent une durée de conservation spécifique.</p>
+
+                 <p>Lorsque vous prenez contact avec <span>DesCodeuses</span> grâce aux différents formulaires de contact, au numéro de téléphone, vos données seront conservées pendant trois (3) ans à compter du dernier échange avec <span>DesCodeuses</span> puis seront supprimées.</p>
+
+                 <h2 class="title2">Quels sont les droits que vous avez sur vos données ?</h2>
+                 <ul>
+                     <li>– le droit d’obtenir la confirmation que vos données sont bien traitées par Le Parcours et les modalités de traitement de ces données,</li>
+                     <li>– le droit d’accéder à vos données,</li>
+                     <li>– le droit de rectifier les données qui sont inexactes ou incomplètes,</li>
+                     <li>– le droit de demander l’effacement des données qui n’ont plus à être conservées (cela ne prend pas en compte les données stockées à des fins administratives, légales ou pour des raisons de sécurité),</li>
+                     <li>– le droit à la limitation du traitement de vos données si vos données étaient utilisées,</li>
+                     <li>– le droit à la notification des mises à jour de vos données,</li>
+                     <li>– le droit à la portabilité des données qui vous avez fournies selon des procédés automatisés (pour les utilisateurs et utilisatrices qui s’enregistrent sur notre site, vous pouvez demander à recevoir un fichier contenant toutes les données personnelles que nous possédons à votre sujet, incluant celles que vous nous avez fournies),</li>
+                     <li>– le droit d’opposition pour motif légitime au traitement de vos données, ou simplement à de la prospection commerciale.</li>
+                 </ul>
+
+                 <p>Vous disposez également du droit de définir des directives relatives au sort de vos données en cas de décès.</p>
+                 <p>Pour exercer ces droits, vous devez en faire la demande, par <strong><a target="_blank"  href="https://wwww.descodeuses.org/index.php#contact">https://wwww.descodeuses.org/index.php#contact</a></strong>  ou par courrier à l’adresse suivante : 37 Rue du Télégraphe 75020, accompagné d’une copie d’un titre d’identité signé.</p>
+
+                 <p>Lorsque le traitement de vos données est effectué sur la base de votre consentement, vous pouvez retirer celui-ci à tout moment. Vous reconnaissez toutefois que les traitements effectués avant la révocation de votre consentement demeurent parfaitement valables.</p>
+
+                 <p>Nous disposons d’un délai d’un mois pour répondre à toute demande relative à l’exercice de vos droits. Ce délai peut être prorogé de deux mois, en raison de la complexité ou du trop grand nombre de demandes.</p>
+
+                 <p>Nous vous rappelons que vous pouvez contacter la CNIL directement sur le site internet <strong><a target="_blank" href="https://www.cnil.fr/fr/agir">https://www.cnil.fr/fr/agir</a></strong>  ou par courrier à l’adresse suivante : Commission Nationale de l’Informatique et des Libertés, 3 Place de Fontenoy – TSA 80715, 75334 PARIS CEDEX 07.</p>
+
+                 <h2 class="title2">Comment vos données sont-elles protégées ?</h2>
+
+                 <p>Vos données sont conservées sur des serveurs sécurisés et protégés par des pare-feu et antivirus.</p>
+
+                 <p>Nous protégeons la sécurité de vos informations personnelles lors de leur transmission en utilisant le logiciel SSL (Secure Sockets Layer Software) qui crypte les informations que vous entrez avant qu’elles ne nous soient envoyées.</p>
+
+                 <p>Nous avons mis en place des mesures techniques et organisationnelles destinées à garantir la sécurité et la confidentialité de vos données contre toute perte accidentelle et contre tout accès, utilisation, modification et divulgation non autorisés.</p>
+
+                 <p>Compte tenu des particularités inhérentes à Internet, il nous est toutefois impossible de garantir la sécurité optimale des échanges d’informations sur ce réseau. Nous nous efforçons de protéger vos données, mais nous ne pouvons garantir la sécurité absolue des informations transmises sur notre site. Vous convenez que vous transmettez vos données à vos propres risques.</p>
+
+                 <p>Nous ne pouvons être tenus responsables du non-respect des paramètres de confidentialité ou des mesures de sécurité en place sur notre site. A ce titre, vous convenez que la sécurité de vos informations est également de votre ressort. Par exemple, il vous incombe de garder secret le mot de passe vous permettant d’accéder à votre compte d’utilisateur. Ne le divulguez en aucun cas à des tiers.</p>
+             </section>
+         </div>
+     </div>
  </main>
 
  <?php
