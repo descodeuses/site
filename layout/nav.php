b15415731aa7e5a5c32bbcaba2a806b350c1a389
mich <michelson@poucedigital.com>
2018-11-07 15:11:52 +0100

    logo + nav order

--- layout/nav.php
+++ layout/nav.php
@@ -2,15 +2,15 @@
     <nav id="site-navigation" class="navbar is-transparent">
         <div class="container">
             <div class="navbar-brand">
-                <a href="<?= RACINE_WEB;?>index.php" class="brand-logo"><img src="assets/images/descodeuses_logo.png" alt=""> </a>
+                <a href="<?= RACINE_WEB;?>index.php" class="brand-logo"><img src="assets/images/descodeuses_logo2.png" alt=""> </a>
             </div>
             <div class="navbar-menu">
                 <div class="navbar-end">
                     <a href="<?= RACINE_WEB;?>index.php" class="navbar-item">Accueil</a>
                     <a href="<?= RACINE_WEB;?>index.php#ambitions" class="navbar-item">Notre ambition</a>
-                    <a href="<?= RACINE_WEB;?>valeurs.php" class="navbar-item">Nos valeurs</a>
                     <a href="<?= RACINE_WEB;?>index.php#actions" class="navbar-item">Actions</a>
                     <a href="<?= RACINE_WEB;?>index.php#equipe" class="navbar-item">Equipe</a>
+                    <a href="<?= RACINE_WEB;?>valeurs.php" class="navbar-item">Nos valeurs</a>
                     <a href="<?= RACINE_WEB;?>index.php#contact" class="navbar-item">Contact</a>
                     <a class="navbar-item">
                         <form id="dons" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
