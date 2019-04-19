c897dc2d01a9cd8ffcde003baf1e8647cee57c97
Mich Henrik <michelson@poucedigital.com>
2019-04-19 09:51:31 +0200

    change link don

--- layout/nav.php
+++ layout/nav.php
@@ -12,13 +12,7 @@
                     <a href="<?= RACINE_WEB;?>index.php#actions" class="navbar-item">Actions</a>
                     <a href="<?= RACINE_WEB;?>index.php#equipe" class="navbar-item">Equipe</a>
                     <a href="<?= RACINE_WEB;?>index.php#contact" class="navbar-item">Contact</a>
-                    <a class="navbar-item">
-                        <form id="dons" action="https://www.paypal.com/cgi-bin/webscr" method="post" target="_top">
-                            <input type="hidden" name="cmd" value="_s-xclick">
-                            <input type="hidden" name="hosted_button_id" value="LYPSS72ZVEK54">
-                            <input type="image" src="http://www.descodeuses.org/assets/images/btn_dons.png" border="1" style="border-radius:3px" name="submit" alt="Button pour les dons">
-                            <img alt="" border="0" src="https://www.paypalobjects.com/fr_FR/i/scr/pixel.gif" width="1" height="1">
-                        </form></a>
+                    <a class="navbar-item button is-info" href="https://www.helloasso.com/associations/descodeuses/formulaires/1" target="_blank">faire un don</a>
                 </div>
             </div>
         </div>
