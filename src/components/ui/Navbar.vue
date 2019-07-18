5ee35aba81404efd1a8ae4aff982c6df843a4851
mich <michelson@poucedigital.com>
2019-07-18 08:15:28 +0200

    Create Navbar.vue

---
+++ src/components/ui/Navbar.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav>
+        <div class="nav-wrapper">
+            <a href="#" class="brand-logo center">DESCODEUSES</a>
+            <ul id="nav-mobile" class="left hide-on-med-and-down">
+                <li><a href="sass.html">DesCodeuses</a></li>
+                <li><a href="badges.html">Nos actions</a></li>
+                <li><a href="collapsible.html">Adhésion</a></li>
+            </ul>
+        </div>
+    </nav>
+</template>
