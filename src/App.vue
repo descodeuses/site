e8bf0576b597c99740ace91a5bccba7f158f41ec
mich <michelson@poucedigital.com>
2019-07-18 08:15:33 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,42 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Navbar/>
+    <img alt="Vue logo" src="./assets/cover_decodeuses.png">
+    <Header/>
+    <Inscription/>
+    <Footer/>
   </div>
 </template>
 
 <script>
 import HelloWorld from './components/HelloWorld.vue'
+import Inscription from './components/forms/Inscription.vue'
+import Footer from './components/ui/Footer.vue'
+import Navbar from './components/ui/Navbar.vue'
+import Header from './components/ui/Header.vue'
+import 'materialize-css'
 
 export default {
   name: 'app',
   components: {
-    HelloWorld
+    HelloWorld,
+    Inscription,
+    Footer,
+    Navbar,
+    Header
   }
 }
 </script>
 
 <style>
+@import '~materialize-css/dist/css/materialize.min.css';
+
+
 #app {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
 }
 </style>
