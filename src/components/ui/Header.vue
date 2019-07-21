616b29850de36dd21f6788b55630175166df7478
mich <michelson@poucedigital.com>
2019-07-21 23:59:26 +0200

    Update Header.vue

--- src/components/ui/Header.vue
+++ src/components/ui/Header.vue
@@ -1,8 +1,18 @@
 <template>
     <header>
-
+        <h2 class="white-text">Descodeuses est une startup sociale qui renforce l’empowerment des femmes des quartiers prioritaires par le numérique. </h2>
+        <button class="waves-effect btn yellow green-text">Savoir plus</button>
     </header>
 </template>
 <style scoped>
+header {
+    min-height: 40vh;
+    background: #000;
+    padding: 50px 0
 
+}
+h2 {
+        margin: 0;
+        padding: 100px
+}
 </style>
