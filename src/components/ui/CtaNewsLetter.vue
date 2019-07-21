6685d7ced2949548ef03a9456c5cd04552859dd3
mich <michelson@poucedigital.com>
2019-07-21 23:59:32 +0200

    Create CtaNewsLetter.vue

---
+++ src/components/ui/CtaNewsLetter.vue
@@ -0,0 +1,27 @@
+<template>
+    <form class="black">
+        <div class="row">
+            <div class="col m4 offset-m2 card">
+                <h2>Inscrivez-vous pour recevoir la newsletter</h2>
+                <div class="input-field col s12">
+                    <input id="email" type="email" class="validate">
+                    <label for="email">E-mail</label>
+                </div>
+                <button class="waves-effect waves-light btn">Envoyer</button>
+            </div>
+        </div>
+    </form>
+</template>
+<style>
+h2 {
+    font-size: 16px !important;
+    text-transform: uppercase
+}
+.card {
+    padding: 30px !important
+}
+.black {
+    padding: 30px 0
+}
+
+</style>
