2b5c1b2311d509ad366b6d3c6a24671853aa46d6
mich <michelson@poucedigital.com>
2019-07-21 23:59:38 +0200

    Create charte.vue

---
+++ src/pages/charte.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h2>Notre Vision</h2>
+        <h3>Toutes les femmes, indépendamment de leurs origines sociales, ont accès aux métiers qui conçoivent le monde de demain.</h3>
+        <h2>Notre Mission</h2>
+        <h3>Développer  le pouvoir d’agir des femmes des quartiers prioritaires en les outillant en les formant et en les accompagnant vers les  métiers valorisés du numérique.</h3>
+
+        <h2>Nos Objectifs/Activités clés</h2>
+        <ul>
+            <li>Sensibiliser les femmes aux métiers en besoin en compétences techniques par des ateliers d’initiation à la programmation.</li>
+            <li>Former les femmes par un parcours complet de 6 mois, à des métiers techniques (développement web, design, traitement de la data).</li>
+            <li>Accompagner les femmes dans leur recherche d’emploi en les préparant à la valorisation de leurs compétences et à la rencontre avec des Entreprises recruteuses.</li>
+            <li>Favoriser l’autonomie numérique et le développement de l’estime de soi des femmes des quartiers par des ateliers de coaching et de mentorat.</li>
+        </ul>
+
+        <h2>Nos  Principes d’action</h2>
+        <h3>respecter le parcours social de chaque femme</h3>
+        <h3>promouvoir l'égalité femmes / hommes</h3>
+        <h3>lutter contre les inégalités de genre et de territoires</h3>
+        <h3>promouvoir la mixité dans le secteur numérique</h3>
+        <h3>s’inscrire dans l’ESS :  gouverner démocratiquement, limiter les écarts de salaires...</h3>
+    </div>
+</template>
