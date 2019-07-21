a78cb29496141a6e3052e95b34082ceaaec80117
mich <michelson@poucedigital.com>
2019-07-21 23:59:40 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,55 @@
-<template>
-  <div id="app">
-    <Navbar/>
-    <img alt="Vue logo" src="./assets/cover_decodeuses.png">
-    <Header/>
-    <Inscription/>
-    <Footer/>
-  </div>
-</template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 import Inscription from './components/forms/Inscription.vue'
 import Footer from './components/ui/Footer.vue'
 import Navbar from './components/ui/Navbar.vue'
 import Header from './components/ui/Header.vue'
+import CtaNewsLetter from './components/ui/CtaNewsLetter.vue'
+import BannerActions from './components/common/BannerActions.vue'
+import BannerPartners from './components/common/BannerPartners.vue'
+import BannerSchedule from './components/common/BannerSchedule.vue'
+import BannerActivities from './components/common/BannerActivities.vue'
 import 'materialize-css'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld,
-    Inscription,
-    Footer,
-    Navbar,
-    Header
-  }
+    name: 'app',
+    components: {
+        Inscription,
+        Footer,
+        Navbar,
+        Header,
+        BannerActions,
+        BannerPartners,
+        BannerSchedule,
+        CtaNewsLetter,
+        BannerActivities
+    },
+    data(){
+        return {
+             component: ''
+        }
+    },
+    methods: {
+        loadComponent(component) {
+            this.component = component
+        }
+    },
 }
 </script>
+
+<template>
+    <div id="app">
+        <Navbar/>
+        <Header/>
+        <BannerPartners/>
+        <BannerActions/>
+        <BannerActivities/>
+        <BannerSchedule/>
+        <Inscription @loadComponent="loadComponent" v-if="component === 'inscription'" />
+        <CtaNewsLetter/>
+        <Footer/>
+    </div>
+</template>
+
 
 <style>
 @import '~materialize-css/dist/css/materialize.min.css';
