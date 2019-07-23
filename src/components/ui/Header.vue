bfa3f93cd4e816da70fec33a2203ccab5dafd174
mich <michelson@poucedigital.com>
2019-07-23 21:41:57 +0200

    Update Header.vue

--- src/components/ui/Header.vue
+++ src/components/ui/Header.vue
@@ -1,18 +1,31 @@
 <template>
-    <header>
-        <h2 class="white-text">Descodeuses est une startup sociale qui renforce l’empowerment des femmes des quartiers prioritaires par le numérique. </h2>
-        <button class="waves-effect btn yellow green-text">Savoir plus</button>
+    <header class="row">
+        <div class="col m4 offset-m8">
+            <h2 class="white-text">Descodeuses est une startup sociale qui renforce l’empowerment des femmes des quartiers prioritaires par le numérique. </h2>
+            <button class="waves-effect btn yellow green-text">Savoir plus</button>
+        </div>
     </header>
 </template>
 <style scoped>
 header {
-    min-height: 40vh;
-    background: #000;
-    padding: 50px 0
-
+    min-height: 80vh;
+    padding: 50px 0;
+    background-image: url("./../../assets/cover_decodeuses.png");
+    background-repeat: no-repeat;
+    background-size: 80%;
+    background-attachment: fixed;
 }
 h2 {
-        margin: 0;
-        padding: 100px
+    margin: 0;
+    padding: 100px
+}
+
+.col {
+    height: 80vh !important;
+    background: #000;
+    padding: 30px!important;
+    -webkit-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+-moz-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
+box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.75);
 }
 </style>
