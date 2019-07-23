866e84f9d7680a91c295168babddeb7027645645
mich <michelson@poucedigital.com>
2019-07-23 21:42:16 +0200

    Update association.vue

--- src/pages/association.vue
+++ src/pages/association.vue
@@ -1,6 +1,13 @@
+<script>
+export default {
+        
+}
+</script>
+
 <template>
     <div>
-        <p>DesCodeuses est une start-up sociale qui construit une société plus égalitaire dans les quartiers prioritaires en accompagnant les femmes vers les métiers du numérique. Nous menons des actions de sensibilisation aux opportunités numériques, des formations aux métiers techniques et un accompagnement personnalisé vers l’emploi. L’innovation sociale et la pédagogie intégrée sont au coeur de notre démarche. </p>
-        <p>DesCodeuses, c’est une formation qui réduit les inégalités de genre et de territoire en répondant aux besoins d’inclusion et de sortie de la précarité pour les femmes des quartiers prioritaires. C’est une formation gratuite, sans pré requis, sans qualification qui promet de la mixité dans le secteur numérique, en ouvrant ses portes à tous ces profils de femmes recherchés par les entreprises. Les emplois créés seront des emplois qualifiés et durables, car se former au numérique, c’est préparer l’avenir en s’armant de compétences modernes. Par ailleurs,  nous préparons les femmes autour de 2 grands axes : un axe d’accompagnement à l’emploi et au marché du travail numérique et un axe de formation aux compétences techniques et transversales. Nos actions de sensibilisation font la promotion de l’égalité femmes/hommes et de rôle modèles féminins dans les filières techniques et technologiques à majorité masculine. </p>
+    
+        <!-- <p>DesCodeuses est une start-up sociale qui construit une société plus égalitaire dans les quartiers prioritaires en accompagnant les femmes vers les métiers du numérique. Nous menons des actions de sensibilisation aux opportunités numériques, des formations aux métiers techniques et un accompagnement personnalisé vers l’emploi. L’innovation sociale et la pédagogie intégrée sont au coeur de notre démarche. </p>
+        <p>DesCodeuses, c’est une formation qui réduit les inégalités de genre et de territoire en répondant aux besoins d’inclusion et de sortie de la précarité pour les femmes des quartiers prioritaires. C’est une formation gratuite, sans pré requis, sans qualification qui promet de la mixité dans le secteur numérique, en ouvrant ses portes à tous ces profils de femmes recherchés par les entreprises. Les emplois créés seront des emplois qualifiés et durables, car se former au numérique, c’est préparer l’avenir en s’armant de compétences modernes. Par ailleurs,  nous préparons les femmes autour de 2 grands axes : un axe d’accompagnement à l’emploi et au marché du travail numérique et un axe de formation aux compétences techniques et transversales. Nos actions de sensibilisation font la promotion de l’égalité femmes/hommes et de rôle modèles féminins dans les filières techniques et technologiques à majorité masculine. </p> -->
     </div>
 </template>
