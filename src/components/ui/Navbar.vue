1005fdc49e032a2c7b9bd2ffcb7ed8925e7a5bc8
mich <michelson@poucedigital.com>
2019-08-07 16:36:53 +0200

    Update Navbar.vue

--- src/components/ui/Navbar.vue
+++ src/components/ui/Navbar.vue
@@ -10,10 +10,10 @@ export default {
     <nav >
         <div class="nav-wrapper white ">
             <ul id="nav-mobile" class="right hide-on-med-and-down">
-                <li><a class="grey-text uppercase" href="#!" data-target="descodeuses">Descodeuses<i class="material-icons right">arrow_drop_down</i></a></li>
-                <li><a class=" grey-text" href="#!" data-target="actions">Nos actions<i class="material-icons right">arrow_drop_down</i></a></li>
-                <li><a class=" grey-text" href="#!" data-target="actions">Contact <i class="material-icons right"></i></a></li>
-                <li @click="$emit('loadComponent', 'inscription')" class="uppercase grey-text">Adhesion<i class="material-icons right"></i></li>
+                <li><a class="grey-text uppercase" href="association" data-target="descodeuses">Descodeuses<i class="material-icons right">arrow_drop_down</i></a></li>
+                <li><a class=" grey-text" href="charte" data-target="actions">Nos actions<i class="material-icons right">arrow_drop_down</i></a></li>
+                <li><a class=" grey-text" href="" data-target="actions">Contact <i class="material-icons right"></i></a></li>
+                <li><a class="grey-text" href="adhesion"> Adhesion<i class="material-icons right"></i></a></li>
                 <li><a class=" grey-text uppercase" href="https://www.helloasso.com/associations/descodeuses/formulaires/1" target="_blank" data-target="adhesion">Dons <i class="material-icons right"></i></a></li>
             </ul>
         </div>
