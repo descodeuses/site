fd7d71bdfb50c4a6569ff80848a747cf660fdd3c
mich <michelson@poucedigital.com>
2019-09-08 19:54:40 +0200

    Update router.js

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from './pages/index.vue'
+import inscription from './pages/inscription.vue'
 
 
 Vue.use(Router)
@@ -12,6 +13,11 @@ export default new Router({
                 path: '',
                 name: 'index',
                 component: index
+            },
+            {
+                path: '/inscription',
+                name: 'inscription',
+                component: inscription
             }
         ]
     })
