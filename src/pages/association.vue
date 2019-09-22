12438bb34e2a1d47ea4aa135a02e1322f8bdf169
mich <michelson@poucedigital.com>
2019-09-22 17:11:46 +0200

    Update association.vue

--- src/pages/association.vue
+++ src/pages/association.vue
@@ -6,8 +6,213 @@ export default {
 
 <template>
     <div>
-    
-        <!-- <p>DesCodeuses est une start-up sociale qui construit une société plus égalitaire dans les quartiers prioritaires en accompagnant les femmes vers les métiers du numérique. Nous menons des actions de sensibilisation aux opportunités numériques, des formations aux métiers techniques et un accompagnement personnalisé vers l’emploi. L’innovation sociale et la pédagogie intégrée sont au coeur de notre démarche. </p>
-        <p>DesCodeuses, c’est une formation qui réduit les inégalités de genre et de territoire en répondant aux besoins d’inclusion et de sortie de la précarité pour les femmes des quartiers prioritaires. C’est une formation gratuite, sans pré requis, sans qualification qui promet de la mixité dans le secteur numérique, en ouvrant ses portes à tous ces profils de femmes recherchés par les entreprises. Les emplois créés seront des emplois qualifiés et durables, car se former au numérique, c’est préparer l’avenir en s’armant de compétences modernes. Par ailleurs,  nous préparons les femmes autour de 2 grands axes : un axe d’accompagnement à l’emploi et au marché du travail numérique et un axe de formation aux compétences techniques et transversales. Nos actions de sensibilisation font la promotion de l’égalité femmes/hommes et de rôle modèles féminins dans les filières techniques et technologiques à majorité masculine. </p> -->
+        <router-view/>
+        <div class="row">
+            <header>
+                <div class="col m8 offset-m2 white back">
+                    <h2>Une start-up sociale.</h2>
+                    <i class="fab fa-adn"></i>
+                    <p class="">Nous sommes une start-up sociale qui réduit les inégalités de genre et de territoire en répondant aux besoin de formation au numérique des femmes issues de quartiers prioritaires de la ville. </p>
+                </div>
+            </header>
+        </div>
+        <div class="row">
+            <div class="col m8 offset-m2 center-align">
+                <h2>DesCodeuses: Formation au numérique pour toutes les femmes</h2>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col m5 card offset-m1">
+                <h2>Vision</h2>
+                <p class="content">Toutes les femmes indépendemment de leur origine social ont accès aux métiers qui définissent le monde de demain. </p>
+            </div>
+            <div class="col m5 card">
+                <h2>Missions</h2>
+                <p class="content">Développer le pouvoir d'agir des femmes des quartiers prioritaires en les outillant, en les formant, en les accompagnant vers les métiers valorisés du numérique. </p>
+            </div>
+        </div>
+        <div class="row left-align">
+            <div class="col m8 offset-m2">
+                <h2 class="center-align">Objectifs</h2>
+                <ol>
+                    <li>Sensibiliser les femmes aux métiers en besoin en compétences techniques par des ateliers d’initiation à la programmation.</li>
+                    <li>Former les femmes par un parcours complet de 6 mois, à des métiers techniques (développement web, design, traitement de la data)</li>
+                    <li>Accompagner les femmes dans leur recherche d’emploi en les préparant à la valorisation de leurs compétences et à la rencontre avec des Entreprises recruteuses</li>
+                    <li>Favoriser l’autonomie numérique et le développement de l’estime de soi des femmes des quartiers par des ateliers de coaching et de mentorat.</li>
+                </ol>
+            </div>
+        </div>
+        <div class="row">
+             <div class="col m4 offset-m1 left-align">
+                <h2 class="">Nos principes d'action</h2>
+                <ul class="left-align">
+                    <li>Respecter le parcours social de chaque femme</li>
+                    <li>Promouvoir l'égalité femmes / hommes</li>
+                    <li>Lutter contre les inégalités de genre et de territoires</li>
+                </ul>
+                 <h2>Notre offre</h2>
+                <ol>
+                    <li>Accès à nos ateliers courts de sensibilisation et de formation au code numérique</li>
+                    <li>Accès à notre formation gratuite de 6 mois de développeuse web</li>
+                    <li>Accès à un accompagnement personnalisé jusqu'à l'emploi</li>
+                    <li>Accès à des ateliers d'empowerment, de coaching, à des conférences avec des professionnels.</li>
+                </ol>
+            </div>
+            <div class="col m6">
+                <img src="@/assets/images/photos/descodeuses-femmes-codent 02.jpg" alt="">
+            </div>
+            <div class="col m4 offset-m1 left-align">
+               
+            </div>
+        </div>
+        <h2 class="heading">L'équipe</h2>
+        <div class="row">
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="@/assets/images/equipe/souad.jpg">
+                        <span class="card-title">Souad</span>
+                    </div>
+                    <div class="card-content">
+                        <i class="fab fa-500px"></i>
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">linkedin</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="@/assets/images/equipe/michelson.jpg">
+                        <span class="card-title">Michelson</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">linkedin</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="@/assets/images/equipe/justine.jpg">
+                        <span class="card-title">Justine</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">linkedin</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="row">
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="@/assets/images/equipe/azul.jpg">
+                        <span class="card-title">Azul</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">linkedin</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="@/assets/images/equipe/olivier.jpg">
+                        <span class="card-title">Olivier</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">linkedin</a>
+                    </div>
+                </div>
+            </div>
+            <div class="col s12 m4">
+                <div class="card">
+                    <div class="card-image">
+                        <img src="@/assets/images/equipe/nolween.jpg">
+                        <span class="card-title">Nolween</span>
+                    </div>
+                    <div class="card-content">
+                        <p>I am a very simple card. I am good at containing small bits of information.
+                        I am convenient because I require little markup to use effectively.</p>
+                    </div>
+                    <div class="card-action">
+                        <a href="#">linkedin</a>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
+<style scoped>
+h2 {
+    font-size: 25px;
+    font-weight: bold;
+    text-transform: uppercase;
+    text-align: center;
+    color: #312A6B
+}
+header {
+    height: 70vh;
+    padding: 50px 0;
+    background-image: url("./../assets/cover_decodeuses.png");
+    background-repeat: no-repeat;
+    background-size: cover;
+    background-attachment: fixed;
+    filter: grayscale(30%);
+}
+
+.white.back {
+    background: #FFF
+}
+
+ul li, ol li {
+    text-transform: initial;
+    padding: 10px
+}
+
+.card {
+    margin: 15px;
+    padding: 0
+}
+.card-title {
+    background: #f9b233
+}
+
+.card h2 {
+    background: #f9b233;
+    margin: 0;
+    padding: 16px;
+    color: #FFF
+}
+
+.content {
+    padding: 16px
+}
+.card-action a {
+    color: #0077B5 !important;
+}
+
+img {
+    width: 100%
+}
+</style>
