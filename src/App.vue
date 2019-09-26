daf3b132c56250342fbd304aea08d8a3a62f01c8
mich <michelson@poucedigital.com>
2019-09-26 22:29:02 +0200

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,7 @@
 <script>
 import Footer from './components/ui/Footer.vue'
 import Navbar from './components/ui/Navbar.vue'
-import Header from './components/ui/Header.vue'
-import CtaNewsLetter from './components/ui/CtaNewsLetter.vue'
-import BannerActions from './components/common/BannerActions.vue'
-import BannerPartners from './components/common/BannerPartners.vue'
-import BannerSchedule from './components/common/BannerSchedule.vue'
-import BannerActivities from './components/common/BannerActivities.vue'
+
 import 'materialize-css'
 
 export default {
@@ -14,12 +9,7 @@ export default {
     components: {
         Footer,
         Navbar,
-        Header,
-        BannerActions,
-        BannerPartners,
-        BannerSchedule,
-        CtaNewsLetter,
-        BannerActivities
+
     },
     data(){
         return {
