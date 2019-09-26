812f033c614be7382ad541a2998467bdb62f12a0
mich <michelson@poucedigital.com>
2019-09-26 22:28:47 +0200

    Update contact.vue

---
+++ src/pages/contact.vue
@@ -0,0 +1,16 @@
+<script>
+import Contact from '@/components/forms/Contact.vue'
+export default {
+    components:{
+        Contact
+    }
+    
+}
+</script>
+
+<template>
+    <div>
+         <router-view/>
+        <Contact/>
+    </div>
+</template>
