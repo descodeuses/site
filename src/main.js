55d8351b979fcb1b563bd98099b648dbf89d7626
mich <michelson@poucedigital.com>
2019-09-27 23:09:57 +0200

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import VeeValidate from 'vee-validate'
+
 Vue.config.productionTip = false
 Vue.use(VeeValidate)
+
 new Vue({
-  router,
-  render: h => h(App),
+    router,
+    render: h => h(App),
 }).$mount('#app')
