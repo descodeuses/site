0e745bb015cbed8ff0311ac3e7ef0d491543197d
mich <michelson@poucedigital.com>
2019-10-08 20:23:46 +0200

    Update aide.vue

---
+++ src/pages/aide.vue
@@ -0,0 +1,24 @@
+<script>
+import Card from '@/components/cards/Card.vue'
+export default {
+    components: {
+        Card
+    },
+    data() {
+        return {
+        }
+    }
+    
+}
+</script>
+<template>
+    <div>
+       <Card
+        :key="index"
+        :heading="'test'"
+        :text="',ljkbhjgfxcvbjnk,l;'"/>
+    </div>
+</template>
+<style scoped>
+
+</style>
