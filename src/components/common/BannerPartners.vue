f95ce4a082ac82b260c4c3bb195e12738885837e
mich <michelson@poucedigital.com>
2019-10-13 14:58:40 +0200

    Update BannerPartners.vue

--- src/components/common/BannerPartners.vue
+++ src/components/common/BannerPartners.vue
@@ -2,22 +2,22 @@
     <div class="container">
         <div class="row">
             <div class="col m2">
-                <img src="@/assets/logos/sap.png" alt="">
+                <img class="responsive-img" src="@/assets/logos/sap.png" alt="">
             </div>
             <div class="col m2">
-                <img src="@/assets/logos/laruche_logo.jpg" alt="">
+                <img class="responsive-img" src="@/assets/logos/laruche_logo.jpg" alt="">
             </div>
             <div class="col m2">
-                <img src="@/assets/logos/projet-19.jpg" alt="">
+                <img class="responsive-img" src="@/assets/logos/projet-19.jpg" alt="">
             </div>
             <div class="col m3 essec">
-                <img src="@/assets/logos/logo_antropia_ESSEC.png" alt="">
+                <img class="responsive-img" src="@/assets/logos/logo_antropia_ESSEC.png" alt="">
             </div>
         </div>
     </div>
 </template>
 <style scoped>
-.container {
+/* .container {
     padding: 3rem
 }
 .row {
@@ -29,5 +29,5 @@
 }
 .essec {
     margin-top: 20px
-}
+} */
 </style>
