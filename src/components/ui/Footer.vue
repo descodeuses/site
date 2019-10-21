b62ecba31a5facb67ff6fefa0f238b8bc77f786a
mich <michelson@poucedigital.com>
2019-10-21 20:38:29 +0200

    Update Footer.vue

--- src/components/ui/Footer.vue
+++ src/components/ui/Footer.vue
@@ -3,7 +3,7 @@
         <div class="container">
             <div class="row">
                 <div class="col l6 s12">
-                    <p class="grey-text text-lighten-4">DesCodeuses est une association loi 1901 sur le numéro W751242699.</p>
+                    <p class="grey-text text-lighten-4">DesCodeuses est une association loi 1901 W751242699.</p>
                     <p>SIRET 84103338400014</p>
                 </div>
                 <div class="col l4 offset-l2 s12">
