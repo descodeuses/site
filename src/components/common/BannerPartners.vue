441971d33703e0e2156093a28c4ca1514f7fb98c
mich <michelson@poucedigital.com>
2019-10-29 17:31:15 +0100

    Update BannerPartners.vue

--- src/components/common/BannerPartners.vue
+++ src/components/common/BannerPartners.vue
@@ -1,22 +1,22 @@
 <template>
     <div class="container">
         <div class="row">
-            <div class="col m2">
+            <div class="col m2 s4">
                 <img class="responsive-img" src="@/assets/logos/sap.png" alt="">
             </div>
-            <div class="col m2">
+            <div class="col m2 s4">
                 <img class="responsive-img" src="@/assets/logos/antropia.png" alt="">
             </div>
-            <div class="col m2">
+            <div class="col m2 s4">
                 <img class="responsive-img" src="@/assets/logos/frenchimpact.png" alt="">
             </div>
-             <div class="col m2">
+             <div class="col m2 s4">
                 <img class="responsive-img" src="@/assets/logos/maisonasso20.png" alt="">
             </div>
-             <div class="col m2">
+             <div class="col m2 s4">
                 <img class="responsive-img" src="@/assets/logos/pariscode.png" alt="">
             </div>
-            <div class="col m2">
+            <div class="col m2 s4">
                 <img class="responsive-img" src="@/assets/logos/paris.png" alt="">
             </div>
         </div>
@@ -34,4 +34,8 @@
     filter: grayscale(80)
 }
 
+
+@media screen and (max-width: 640px) {
+
+ }
 </style>
