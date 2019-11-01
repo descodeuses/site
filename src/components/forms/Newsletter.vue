1f8eea8542e38f3e45190ee7c6cb0895e3e2c082
mich <michelson@poucedigital.com>
2019-11-01 17:15:54 +0100

    Create Newsletter.vue

---
+++ src/components/forms/Newsletter.vue
@@ -0,0 +1,26 @@
+<script>
+export default {
+    data() {
+        return {
+            inscription: {}
+        }
+    }
+}
+</script>
+<template>
+    <form>
+        <h2 class="heading">Recevez notre newsletter</h2>
+        <div class="input-field col s12">
+            <input 
+                id="email" 
+                type="email" 
+                class="validate"
+                v-model="inscription.newletter">
+            <label for="email">E-mail</label>
+        </div>
+        <button class="waves-effect waves-light btn"
+        type="submit">Envoyer</button>
+    </form>
+</template>
+<style scoped>
+</style>
