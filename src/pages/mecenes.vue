7f8e18e0b2eb8ee77778a1811c1e1ba36ea3c5c9
mich <michelson@poucedigital.com>
2019-11-20 20:06:31 +0100

    Create mecenes.vue

---
+++ src/pages/mecenes.vue
@@ -0,0 +1,19 @@
+<template>
+    <div>
+        <ul>
+            <li>Niakalé Sissoko</li>
+            <li>Vanessa	Doux</li>
+            <li>Yann Mauchamp</li>
+            <li>Yilor Lyfoung</li>
+            <li>Fatima Debbah</li>
+            <li>Sarah Adel</li>
+            <li>Christian Kamtchueng</li>
+            <li>Florence Philipponnat</li>
+            <li>Aminata Diakhaby</li>
+            <li>Ivan Piettre</li>
+            <li>Farha Bouziar</li>
+            <li>Florence Philipponnat</li>
+            <li>Ashok Bartwal</li>
+        </ul>
+    </div>
+</template>
