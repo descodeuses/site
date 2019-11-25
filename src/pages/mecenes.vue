f03e77016d97a51167157ff178b69190377033cb
mich <michelson@poucedigital.com>
2019-11-25 19:34:02 +0100

    Update mecenes.vue

--- src/pages/mecenes.vue
+++ src/pages/mecenes.vue
@@ -1,19 +1,42 @@
 <template>
-    <div>
-        <ul>
-            <li>Niakalé Sissoko</li>
-            <li>Vanessa	Doux</li>
-            <li>Yann Mauchamp</li>
-            <li>Yilor Lyfoung</li>
-            <li>Fatima Debbah</li>
-            <li>Sarah Adel</li>
-            <li>Christian Kamtchueng</li>
-            <li>Florence Philipponnat</li>
-            <li>Aminata Diakhaby</li>
-            <li>Ivan Piettre</li>
-            <li>Farha Bouziar</li>
-            <li>Florence Philipponnat</li>
-            <li>Ashok Bartwal</li>
-        </ul>
+    <div class="container">
+        <div class="row">
+            <div class="col m6 left-align">
+                <h2 class="heading left-align">Particuliers</h2>
+                <p>Merci pour vos dons</p>
+                <ul>
+                    <li>Niakalé Sissoko</li>
+                    <li>Vanessa	Doux</li>
+                    <li>Yann Mauchamp</li>
+                    <li>Yilor Lyfoung</li>
+                    <li>Fatima Debbah</li>
+                    <li>Sarah Adel</li>
+                    <li>Christian Kamtchueng</li>
+                    <li>Florence Philipponnat</li>
+                    <li>Aminata Diakhaby</li>
+                    <li>Ivan Piettre</li>
+                    <li>Farha Bouziar</li>
+                    <li>Florence Philipponnat</li>
+                    <li>Ashok Bartwal</li>
+                </ul>
+            </div>
+            <div class="col m6 left-align">
+                <h2 class="heading left-align">Entreprises</h2>
+                <p>Merci aux dons materiaux, et tous les sorts de soutient</p>
+                <ul>
+                    <li> (pour les ordinateurs</li>
+                    <li>Evaneos (d'avoir preter vos locaux pour faire nos photos)</li>
+                </ul>
+
+            </div>
+
+        </div>
+        
     </div>
 </template>
+
+<style lang="scss" scoped>
+h2 {
+    text-align: left
+}
+</style>
