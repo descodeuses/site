0a6496034c0d137b1e0c25432415bcca1b87e0ab
mich <michelson@poucedigital.com>
2019-11-26 23:03:08 +0100

    Create shop.vue

---
+++ src/pages/shop.vue
@@ -0,0 +1,83 @@
+<template>
+<div class="row">
+    <div class="col m10 offset-m1">
+        <h1 class="title">Boutique Descodeuses</h1>
+        <div class="col m4">
+              <div class="card">
+                <div class="card-image waves-effect waves-block waves-light">
+                    <img src="@/assets/images/shop/descodeuses-mug.jpg" alt="">
+                </div>
+                <div class="card-content">
+                    <span class="small-title">Mug</span> 
+                    <h2 class="card-title activator grey-text text-darken-4">"Tech The Power"</h2>
+                        <span class="helper-text"> by Descodeuses<i class="material-icons right">more_vert</i></span>
+                    <p><a href="https://www.helloasso.com/associations/descodeuses/paiements/mug-code-without-gender-by-descodeuses">Acheter</a></p>
+                </div>
+                <div class="card-reveal">
+                    <span class="card-title grey-text text-darken-4">
+                        Mug
+                        <i class="material-icons right">close</i>
+                    </span>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col m4">
+              <div class="card">
+                <div class="card-image waves-effect waves-block waves-light">
+                    <img src="@/assets/images/shop/descodeuses-code-without-gender.jpg" alt="">
+                </div>
+                <div class="card-content">
+                    <span class="small-title">T-shirt</span> 
+                    <h2 class="card-title activator grey-text text-darken-4">"Code Without Gender"</h2>
+                    <span class="helper-text"> by Descodeuses<i class="material-icons right">more_vert</i></span>
+                    <p><a href="https://www.helloasso.com/associations/descodeuses/paiements/mug-code-without-gender-by-descodeuses">Acheter</a></p>
+                </div>
+                <div class="card-reveal">
+                    <span class="card-title grey-text text-darken-4"> T-shirt
+                        <i class="material-icons right">close</i>
+                    </span>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+        <div class="col m4">
+              <div class="card">
+                <div class="card-image waves-effect waves-block waves-light">
+                    <img src="@/assets/images/shop/descodeuses-tech-the-power.jpg" alt="">
+                </div>
+                <div class="card-content">
+                    <span class="small-title">T-shirt</span> 
+                    <h2 class="card-title activator grey-text text-darken-4">"Tech The Power"</h2>
+                    <span class="helper-text"> by Descodeuses<i class="material-icons right">more_vert</i></span>
+                    <p><a href="https://www.helloasso.com/associations/descodeuses/paiements/t-shirt-tech-the-power-by-descodeuses">Acheter</a></p>
+                </div>
+                <div class="card-reveal">
+                    <span class="card-title grey-text text-darken-4">T-shirt<i class="material-icons right">close</i></span>
+                    <ul>
+                        <li></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+</template>
+
+<style lang="scss" scoped>
+.title {
+    font-size: 2em
+}
+.card {
+    .card-image {
+        height: 350px;
+    }
+    .small-text {
+        font-size: 0.9em
+    }
+}
+</style>
