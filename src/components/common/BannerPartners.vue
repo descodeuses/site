04477361ed7c431fef337433cff28a97b93247d7
mich <michelson@poucedigital.com>
2020-01-23 19:48:25 +0100

    Update BannerPartners.vue

--- src/components/common/BannerPartners.vue
+++ src/components/common/BannerPartners.vue
@@ -1,37 +1,31 @@
 <template>
-    <div class="container">
-        <div class="row">
-            <div class="col m2 s4">
-                <img class="responsive-img" src="@/assets/logos/sap.png" alt="">
-            </div>
-            <div class="col m2 s4">
-                <img class="responsive-img" src="@/assets/logos/antropia.png" alt="">
-            </div>
-            <div class="col m2 s4">
-                <img class="responsive-img" src="@/assets/logos/frenchimpact.png" alt="">
-            </div>
-             <div class="col m2 s4">
-                <img class="responsive-img" src="@/assets/logos/maisonasso20.png" alt="">
-            </div>
-             <div class="col m2 s4">
-                <img class="responsive-img" src="@/assets/logos/pariscode.png" alt="">
-            </div>
-            <div class="col m2 s4">
-                <img class="responsive-img" src="@/assets/logos/paris.png" alt="">
+    <div class="partners">
+        <div class="row center">
+            <div class="col offset-m1 center">
+                <img class="responsive-img sap" src="@/assets/logos/fondation_sap.png" alt="Logo SAP">
+                <img class="responsive-img sap" src="@/assets/logos/google.png" alt="Logo SAP">
+                <img class="responsive-img" src="@/assets/logos/antropia.png" alt="Logo Antropia">
+                <img class="responsive-img" src="@/assets/logos/pariscode.png" alt="Logo Paris Code">
+                <img class="responsive-img" src="@/assets/logos/frenchimpact.png" alt="Logo French Impact">
+                
+                <img class="responsive-img" src="@/assets/logos/maisonasso20.png" alt="Logo Maison des Asso du 20e">
+                <img class="responsive-img" src="@/assets/logos/paris.png" alt="Logo ville de Paris">
             </div>
         </div>
     </div>
 </template>
 <style scoped>
-.container {
-    padding: 3rem
+.partners {
+    padding: 3rem;
+    text-align: center
 }
 .row {
     vertical-align: middle;
 }
-.container img {
-    width: 100%;
-    filter: grayscale(80)
+.partners img {
+    width: 150px;
+    filter: grayscale(80);
+    margin: 0 10px
 }
 
 
